41daa9d84b21d8dcd0eb8afc59f45977d06c3c99
lillian039 <1912383615@qq.com>
2022-11-14 18:54:13 +0800

    update structure

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-## RISC-V Tomasulo CPU Simulatior 🧐
+# RISC-V Tomasulo CPU Simulatior 🧐
 
-#### CPU初步架构图：
+#### 目录：
+
+[TOC]
+
+## PART0 大致框架
+
+### CPU初步架构图：
 
 ![638ab07256c710c87647b9e7305b3b2.jpg](https://github.com/lillian039/RISCV-CPU-2022/blob/main/README.assets/638ab07256c710c87647b9e7305b3b2.jpg?raw=true)
 
-#### 所需模块
+### 所需模块
 
 - Register
 - Reorder_Buffer
@@ -16,3 +22,25 @@
 - ALU
 - Decoder
 - Branch_Target_Buffer
+
+## PART1 Instruction Fetch
+
+### 图示 Instruction Fetch
+
+<img src="D:\a_homework_of_c++\2-1_semester\RISCV-CPU-2022\README.assets\997af3ed740d718f7f89832a35714ad.jpg" alt="997af3ed740d718f7f89832a35714ad" style="zoom:33%;" />
+
+### 所需模块：
+
+#### （1） Icache
+
+instruction cache，暂定大小64，由于大小较小，暂定使用 Fully Associated Cache
+
+#### （2）Instruction Queue
+
+pc 指针从 cache 或 RAM 中拿到的数据先放在 instruction queue 中，rob直接从 instruction queue 拿指令
+
+#### （3）Branch Target Buffer
+
+用于分支预测 与 ISQ、ROB 和 PC 相连 若 ISQ 拿到的指令与jump有关，就放进 BTB 判
+
+跳就更新为 target PC，否则 PC+=4
--- riscv/src/branch_target_buffer.v
+++ riscv/src/branch_target_buffer.v
@@ -1,3 +1,49 @@
-module branch_target_buffer('
+`define    stronglyNotTaken     2'b00;
+`define    weaklyNotTaken       2'b01;
+`define    weaklyTaken          2'b10;
+`define    stronglyTaken        2'b11;
+
+module branch_target_buffer(
+    input   wire            clk_in,                 //  system clock signal
+    input   wire            rst_in,                 //  reset signal
+    input   wire            rdy_in,                 //  ready signal, pause cpu when low
+    input   wire            judge_btb,              //  whether jump
+    input   wire            change_state,           //  whether change state
+    input   wire            result,                 //change pc result
+    input   wire    [31:0]  judge_jump_pc_in,       //whether pc need jump
+    input   wire    [31:0]  change_state_pc_in,     //change pc state
+    output  wire            jump_out,         
 );
+parameter   BTBSIZE=64;
+parameter prime=337;
+reg [3:0]   btb_single  [BTBSIZE-1:0];
+
+integer i;
+integer hash_idx_jump;
+integer hash_idx_change;
+reg     whether_jump;
+assign  jump_out=whether_jump;
+always @(posedge clk_in) begin
+    if (rst_in)begin//清空btb
+    for(i=0;i<BTBSIZE;i=i+1)begin
+        btb_single[i]=weaklyNotTaken;
+    end
+    end
+    else if(!rdy_in)begin//低信号或没有需要判断的jump  pause
+    end
+    else begin
+    if (judge_btb)begin 
+        hash_idx_jump=(judge_jump_pc_in*prime)%BTBSIZE;
+        if(btb_single[hash_idx_jump]==weaklyTaken||btb_single[hash_idx_jump]==stronglyTaken)whether_jump=TRUE;
+        else whether_jump=FALSE;
+    end
+    if (change_state)begin
+        hash_idx_change=(change_state_pc_in*prime)%BTBSIZE;
+        if(result==TRUE && btb_single[hash_idx_change]!=stronglyTaken) btb_single[hash_idx_change] = btb_single[hash_idx_change]+1;
+        else if(result==FALSE && btb_single[hash_idx_change]!=stronglyNotTaken) btb_single[hash_idx_change] = btb_single[hash_idx_change]-1;
+    end
+
+    end
+
+end
 endmodule
